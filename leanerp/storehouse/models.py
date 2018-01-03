727a474d0e697b473631ee2343cc41120985a5c0
seLain <selain@nature.ee.ncku.edu.tw>
2018-01-03 15:40:01 +0800

    commit missed files

---
+++ leanerp/storehouse/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+class Goods(models.Model):
+
+	barcode = models.TextField(blank=True, primary_key=True)
+	name = models.TextField(blank=True)
+	internal_code = models.TextField(blank=True)
+	last_update_date = models.DateTimeField(default=datetime(1900, 1, 1))
+
+	# overwrite save to force last_update_date updated when save()
+	def save(self, *args, **kwargs):
+		self.last_update_date = datetime.now()
+		super(Goods, self).save(*args, **kwargs)
+
+class Supplier(models.Model):
+
+	internal_code = models.TextField(blank=True, primary_key=True)
+	name = models.TextField(blank=True)
+	phone = models.TextField(blank=True)
+
